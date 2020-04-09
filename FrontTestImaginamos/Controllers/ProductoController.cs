3dd8efe927a9ac71db61ace33bec045aedc1f136
Alejandro Martinez <maximus.jandos.joya@gmail.com>
2020-04-09 17:15:58 -0500

    Creacion de vista de lista de productos

---
+++ FrontTestImaginamos/Controllers/ProductoController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FrontTestImaginamos.Controllers
+{
+    public class ProductoController : Controller
+    {
+        private readonly BackWebServices.MainWebService webService = new BackWebServices.MainWebService();
+        // GET: Producto
+        public ActionResult Index()
+        {
+            var listaProductos = webService.ListarProductos().ToList();
+            return View(listaProductos);
+        }
+
+        // GET: Producto/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Producto/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
